0c2d292f08d2df4539745f5e1bfb34dd4fa6c925
afrasiabi1374 <mohammad.aligiga@gmail.com>
2021-12-14 15:57:41 +0330

    v-model on component

---
+++ components/UI/AppTextInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      title : <input type="text" v-on="{...$listeners,input}">
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        value:{
+            type:String,
+            default:''
+        }
+    },
+    methods:{
+        input(e){
+            this.$emit('input',e.target.value)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/form.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <app-text-input v-model="title"></app-text-input>
+  </div>
+</template>
+
+<script>
+import AppTextInput from '../components/UI/AppTextInput.vue'
+export default {
+    components: {
+        AppTextInput
+    },
+    data(){
+        return{
+            title:''
+        }
+    },
+    watch:{
+        title(newVal){
+            console.log('the value of title is ==>>>>>',newVal)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
